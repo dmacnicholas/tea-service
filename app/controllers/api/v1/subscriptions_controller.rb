6f0ec237bc82e156812c98df11ccb794050005c1
Drew MacNicholas <dmacnicholas@gmail.com>
2022-11-03 22:08:59 -0500

    Update routes

---
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -0,0 +1,8 @@
+class Api::V1::SubscriptionsController < ApplicationController
+
+  def index
+    customer = Customer.find(params[:customer_id])
+    render json: SubscriptionSerializer.new(customer.subscriptions)
+  end
+
+end  
