9a696371309dfb5d8227e01714664d2f432bcf02
Drew MacNicholas <dmacnicholas@gmail.com>
2022-11-04 00:22:50 -0500

    Add in request methods for create and index

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -5,4 +5,14 @@ class Api::V1::SubscriptionsController < ApplicationController
     render json: SubscriptionSerializer.new(customer.subscriptions)
   end
 
-end  
+  def create
+    customer = Customer.find(params[:customer_id]) && Tea.find(params[:tea_id])
+    render json: SubscriptionSerializer.new(Subscription.create(subscription_params)), status: :created
+  end
+
+private
+
+  def subscription_params
+    params.permit(:title, :price, :status, :frequency, :tea_id, :customer_id)
+  end
+end
