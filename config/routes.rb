bbc43d7edb9fc8d849f971265a5b950add1541d1
Drew MacNicholas <dmacnicholas@gmail.com>
2022-11-04 00:23:16 -0500

    Add route for create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resource :subscriptions, only: [:index, :create, :update]
+         get '/subscriptions', to: 'subscriptions#index'
+         post '/subscriptions', to: 'subscriptions#create'
     end
   end
 end
